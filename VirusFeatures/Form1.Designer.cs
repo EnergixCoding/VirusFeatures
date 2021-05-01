b80c4be19ffed328f71af03f97ac605ceb70ed88
EnergixCoding <dogloof11@gmail.com>
2021-05-01 17:27:55 +0100

    Add project files.

---
+++ VirusFeatures/Form1.Designer.cs
@@ -0,0 +1,48 @@
+﻿
+namespace VirusFeatures
+{
+    partial class MalwareFeatures
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MalwareFeatures
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "MalwareFeatures";
+            this.Text = "Malware Features v1.0 - By EnergixCoding";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
